4729962a89e19005825e4a0b95df2708286cbc30
Chinedu Anene <caanenedr@outlook.com>
2019-10-30 11:57:16 +0000

    Update Primer_effic.R

--- primer_efficy/Primer_effic.R
+++ primer_efficy/Primer_effic.R
@@ -1,12 +1,18 @@
-# Function to caculate primer efficeincy 
+#!/usr/bin/env Rscript
+# Script to caculate primer efficeincy 
 # Assumes a df of at least three columns:
 #                                    Sample.Name : cDNA dilution "1:x"
 #                                    CT          : qPCR CT value
 #                                    Target.Name : Name of gene "primer_pair"
-primer_efficy <- function(x,H2O_name, sep_dilution) {
+# Useage: Rscript Primer_effic.R ~/Desktop/prime_eff.csv h2o :
+# 
+main <- function() {
+  args <- commandArgs(trailingOnly = TRUE)
+  stopifnot(length(args) == 3)
+  x <- read.csv(args[1])
+  #H2O_name <- args[2]
+  #sep_dilution <- args[3]
   # These names correspond to the output of a specific machine
-  # Check your output
-  #
   dat <- x[c("Sample.Name", "CT", "Target.Name")]
   # Set the id of the Target name to simplify things in the feature
   # Process
@@ -20,12 +26,11 @@ primer_efficy <- function(x,H2O_name, sep_dilution) {
   umods <- lapply(forma, function(x){subset(dat, Target.Name == x)})
   
   # Prepare aggregation function
-   
+  
   # Function to force numeric
   mean_convert <- function(x) {
     mean(as.numeric(as.character(x)))
   }
-  
   
   # Function to subset only numeric variables
   numeric_columns <- function(x){
@@ -33,14 +38,13 @@ primer_efficy <- function(x,H2O_name, sep_dilution) {
     return(x[ , num_col])
   }
   
-  
   # Function to aggregate CT values by mean
   # You may Use 
   aggregate_CT <- function(x) {
     # Configure for quntstudio's output "Sample.Name, Target.Name"
     x_num <- numeric_columns(x)
     aggregated <- aggregate(x_num, by = list(x$Sample.Name),
-              FUN = mean_convert)
+                            FUN = mean_convert)
     names(aggregated) <- c("Sample_", "CT_mean")
     aggregated$Target <- unique(x$Target.Name)
     return(aggregated)
@@ -51,8 +55,8 @@ primer_efficy <- function(x,H2O_name, sep_dilution) {
   
   # Function to format the dultion
   create_dilute <- function(x){
-    xx <- subset(x, !Sample_ == H2O_name)
-    xx$Dil <- gsub(paste("*.", sep_dilution, sep = ""), "", xx$Sample_)
+    xx <- subset(x, !Sample_ == args[2])
+    xx$Dil <- gsub(paste("*.", args[3], sep = ""), "", xx$Sample_)
     xx$s_Dil <- log(1/as.numeric(xx$Dil))
     return(xx)
   }
@@ -62,17 +66,15 @@ primer_efficy <- function(x,H2O_name, sep_dilution) {
   
   # Calculate efficiency
   res <- lapply(ad_dilute,
-                              function(x){s <- lm(CT_mean ~ s_Dil, x)$coefficients[2]
-                              s <- 10^(-1/s)
-                              s <- (s - 1)*100
-                              s <- paste(unique(x$Target), s, sep = " : ")
-                              })
+                function(x){s <- lm(CT_mean ~ s_Dil, x)$coefficients[2]
+                s <- 10^(-1/s)
+                s <- (s - 1)*100
+                s <- paste(unique(x$Target), s, sep = " : ")
+                })
   # Print results
   for (i in res) {
-    print(i)
+    cat(i, sep = "\n")
   }
 }
 
-# Calculate example
-foo <- read.csv("~/Desktop/prime_eff.csv")
-primer_efficy(foo, "h2o", ":")
+main()
